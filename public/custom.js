a5e97f307463213640b7bcfabaaa5489e5d23ab7
Gilhyeon Gim <rlfgus777@gmail.com>
2017-04-09 04:44:53 +0000

    first

---
+++ public/custom.js
@@ -0,0 +1,8 @@
+/*global $*/
+ $(".button-collapse").sideNav();
+ 
+ function fnMove(seq){
+        var offset = $("#div" + seq).offset();
+        $('html, body').animate({scrollTop : offset.top}, 400);
+    };
+
